a5983cc9eeec65f7fde82fec9d95d85322df7224
Dmi_Krop <melomanko@gmail.com>
2020-12-06 03:40:51 +0700

    add inheritance for classes

--- lesson6_hw1.py
+++ lesson6_hw1.py
@@ -7,62 +7,83 @@ class Animals:
         self.weight += value
         return "покормили"
 
+    def do_voice(self):
+        pass
+
+    def do_personal_action(self):
+        pass
+
 
 class EggLaying(Animals):  # несущие яйца
-    def do_smth(self):
-        return "которое несет яйца"
+    def do_eggs(self):
+        return "несет яйца"
+
+    def do_personal_action(self):
+        return self.do_eggs() + ", " + self.do_voice()
+
+
+class Shearing(Animals):  # Стригущиеся
+    def do_shave(self):
+        return "cтрижется"
+
+    def do_personal_action(self):
+        return self.do_shave() + ", " + self.do_voice()
+
+
+class Milking(Animals):  # доящиеся
+    def do_milk(self):
+        return "доится"
+
+    def do_personal_action(self):
+        return self.do_milk() + ", " + self.do_voice()
 
 
 class Chicken(EggLaying):
     representative = "Курица"
 
     def do_voice(self):
-        return "Кококо"
+        return "говорит Кококо"
 
 
 class Goose(EggLaying):
     representative = "Гусь"
 
     def do_voice(self):
-        return "Га-га"
+        return "говорит Га-га"
 
 
 class Duck(EggLaying):
     representative = "Утка"
 
     def do_voice(self):
-        return "Кря-кря"
+        return "говорит Кря-кря"
 
 
-class Shearing(Animals):  # Стригущиеся
-    def do_smth(self):
-        return "которое cтрижется"
-
-
-class Sheep(Shearing):
+class Sheep(Shearing, Milking):
     representative = "Овца"
 
     def do_voice(self):
-        return "Бееее"
+        return "говорит Бееее"
 
-
-class Milking(Animals):  # доящиеся
-    def do_smth(self):
-        return "которое доится"
+    def do_personal_action(self):
+        return self.do_shave() + " и " + self.do_milk() + ", " + self.do_voice()
 
 
 class Cow(Milking):
     representative = "Корова"
 
     def do_voice(self):
-        return "Муууу"
+        return "говорит Муууу"
 
 
-class Goat(Milking):
+class Goat(Milking, Shearing):
     representative = "Коза"
 
     def do_voice(self):
-        return "Мееее"
+        return "говорит Мееее"
+
+    def do_personal_action(self):
+        return self.do_shave() + " и " + self.do_milk() + ", " + self.do_voice()
 
 
 feed_value = 2
@@ -72,9 +93,9 @@ farm = [
     Goose("Белый", 12),
     Goose("Серый", 14),
     Duck("Кряква", 8),
-    Sheep("Барашек", 80),
-    Sheep("Кудрявый", 90),
-    Cow("Манька", 150),
+    Sheep("Шон", 80),
+    Sheep("Долли", 90),
+    Cow("Лепешка", 150),
     Goat("Дереза", 60),
     Goat("Копытце", 70),
 ]
@@ -82,7 +103,7 @@ farm = [
 max_weight = sum_weight = 0
 for animal in farm:
     print(
-        f'{animal.representative} "{animal.name}" - это животное, {animal.do_smth()} и говорит {animal.do_voice()}, вес = {animal.weight}, и мы его {animal.feed(feed_value)} на {feed_value}'
+        f'{animal.representative} "{animal.name}" - это животное, которое {animal.do_personal_action()}, вес = {animal.weight}, и мы его {animal.feed(feed_value)} на {feed_value}'
     )
     sum_weight += animal.weight
     if animal.weight > max_weight:
